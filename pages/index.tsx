9ef2f53f2740f5e3e0b931cc960466d1fc5edbe5
icepy <xiangwenwe@gmail.com>
2022-09-23 11:28:55 +0800

    chore: config alchemy and update docs

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,19 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { ethers } from 'ethers';
+import { useEffect } from 'react';
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    const NEXT_PUBLIC_ALCHEMY = process.env.NEXT_PUBLIC_ALCHEMY;
+    const NEXT_PUBLIC_TESTHASH = process.env.NEXT_PUBLIC_TESTHASH;
+    console.log('NEXT_PUBLIC_ALCHEMY', NEXT_PUBLIC_ALCHEMY);
+    const provider = new ethers.providers.AlchemyProvider('arbitrum', NEXT_PUBLIC_ALCHEMY);
+    provider.getTransaction(NEXT_PUBLIC_TESTHASH as string).then((res) => {
+      console.log(res)
+    });
+  }, [])
   return (
     <div className="">
       
