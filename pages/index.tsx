9d39c2d7e943f8a3ad5de033e3945e8da40fa8ae
icepy <xiangwenwe@foxmail.com>
2022-12-15 11:49:33 +0800

    chore: 接口限制小于 2023

--- pages/index.tsx
+++ pages/index.tsx
@@ -82,7 +82,7 @@ const Home: NextPage = () => {
         <div>
           <div className="form-control">
             <div className="input-group">
-              <input type="text" value={address} placeholder="Search Address…" className="input input-bordered" onChange={inputHandler}/>
+              <input type="text" value={address} placeholder="Search Address…" className="input input-bordered w-max" onChange={inputHandler}/>
               <button className="btn btn-square" onClick={submitHandler}>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
               </button>
@@ -112,7 +112,7 @@ const Home: NextPage = () => {
                 </tr>
                 <tr>
                   <td>PROFIT</td>
-                  <td>{ethers.utils.formatEther(s)}</td>
+                  <td className="font-black">{ethers.utils.formatEther(s)}</td>
                 </tr>
               </tbody>
             </table>
